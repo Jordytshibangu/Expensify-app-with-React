1004d39e00625d816709240d49a470f27b78ff7f
MACBOOK PRO <macbookpro@MACBOOKs-MacBook-Pro.local>
2019-05-15 12:18:57 +0200

    setup formatting for amount and created at

--- src/component/ExpenseListItem.js
+++ src/component/ExpenseListItem.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
+import numeral from 'numeral'
 
 
 const ExpenseListItem = ({ id, description, amount , createAt}) => (
@@ -8,9 +10,11 @@ const ExpenseListItem = ({ id, description, amount , createAt}) => (
         <h3>{description}</h3>
     </Link>
         
-        <p> {amount} - {createAt} </p>
-       
-    
+        <p> 
+        {numeral(amount / 100).format('$0,0.00')} 
+        -
+        {moment(createAt).format('MMMM Do, YYYY')} 
+        </p>
     </div>
 );
 export default ExpenseListItem
